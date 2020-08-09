bc8ae79795f6c47a36a0bf9f100d4a43b99aa77c
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-09 11:55:57 +0530

    Create AddingTwoNumbers.java
    here given a java program for adding two numbers

---
+++ AddingTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.reshnu;
+
+public class AddingTwoNumbers{
+    public void sum(int a,int b){
+        int sum;
+        sum=a+b;
+        System.out.println(sum);
+    }
+    public static void main(String [] args){
+        AddingTwoNumbers obj=new AddingTwoNumbers();
+       obj.sum(50,20);
+
+    }
+
+}
